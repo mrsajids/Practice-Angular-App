1bc070a90cd3ec1c278e147523f8a894f1cfe055
Sajeed <syedsajeed6969@gmail.com>
2024-08-25 15:31:35 +0530

    install boostrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'feature', loadChildren: () => import('./feature/feature.module').then(m => m.FeatureModule) },
-    {
-        path: '',
-        loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule)
-    },
-    {
-        path: '',
-        loadChildren: () => import('./holiday/holiday-routing.module').then(m => m.HolidayRoutingModule)
-    },
     {
         path: 'parent',
         loadChildren: () => import('./parent/parent-routing.module').then(m => m.ParentRoutingModule)
